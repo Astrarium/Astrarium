973fed74aeaa5c2032cad76fbaf3b8803195225b
Alexander Krutov <krutov@tecomgroup.ru>
2019-02-08 16:46:25 +0300

    Some fixes related to incorrect track path drawing

--- ADK.Demo/Renderers/TrackRenderer.cs
+++ ADK.Demo/Renderers/TrackRenderer.cs
@@ -30,8 +30,15 @@ namespace ADK.Demo.Renderers
         public override void Render(Graphics g)
         {
             var tracks = Sky.Get<ICollection<Track>>("Tracks");
+
             foreach (var track in tracks)
             {
+                PointF pBody = (Sky.Context.JulianDay > track.FromJD &&
+                    Sky.Context.JulianDay < track.ToJD &&
+                    Angle.Separation(track.Body.Horizontal, Map.Center) < Map.ViewAngle * 1.2) ?
+                 Map.Projection.Project(track.Body.Horizontal) :
+                 PointF.Empty;
+
                 var segments = track.Points
                     .Select(p => Angle.Separation(p.Horizontal, Map.Center) < Map.ViewAngle * 1.2 ? p : null)
                     .Split(p => p == null, true);
@@ -50,7 +57,7 @@ namespace ADK.Demo.Renderers
                         segment.Add(nextP);
                     }
 
-                    DrawTrackSegment(g, penTrack, segment.Select(p => Map.Projection.Project(p.Horizontal)).ToArray());
+                    DrawTrackSegment(g, penTrack, segment.Select(p => Map.Projection.Project(p.Horizontal)).ToArray(), pBody);
                 }
 
                 if (!segments.Any())
@@ -72,7 +79,7 @@ namespace ADK.Demo.Renderers
                         segment.Add(p2);
                     }
 
-                    DrawTrackSegment(g, penTrack, segment.Select(p => Map.Projection.Project(p.Horizontal)).ToArray());
+                    DrawTrackSegment(g, penTrack, segment.Select(p => Map.Projection.Project(p.Horizontal)).ToArray(), pBody);
                 }
 
                 DrawLabels(g, track);
@@ -108,7 +115,7 @@ namespace ADK.Demo.Renderers
             }
         }
 
-        private void DrawTrackSegment(Graphics g, Pen penGrid, PointF[] points)
+        private void DrawTrackSegment(Graphics g, Pen penGrid, PointF[] points, PointF pBody)
         {
             // Do not draw figure containing less than 2 points
             if (points.Length < 2)
@@ -162,7 +169,7 @@ namespace ADK.Demo.Renderers
                             newPoints.Add(p2);
                         }
 
-                        DrawTrackSegment(g, penGrid, newPoints.ToArray());
+                        DrawTrackSegment(g, penGrid, newPoints.ToArray(), pBody);
                     }
 
                     if (!segments.Any())
@@ -181,15 +188,50 @@ namespace ADK.Demo.Renderers
                             newPoints.Add(p2);
                         }
 
-                        DrawTrackSegment(g, penGrid, newPoints.ToArray());
+                        DrawTrackSegment(g, penGrid, newPoints.ToArray(), pBody);
                     }
                 }
             }
             // More or equal than 7 points draw the curve in regular way
             else
             {
+                if (pBody != PointF.Empty)
+                {
+                    g.DrawCurve(Pens.Red, points);
+
+                    PointF[] nearest = points.OrderBy(p => Geometry.DistanceBetweenPoints(p, pBody)).Take(2).ToArray();
+                    if (Geometry.DistanceBetweenPoints(nearest[0], nearest[1]) > 1)
+                    {
+                        PointF proj = getProjectedPointOnLineFast(nearest[0], nearest[1], pBody);
+
+                        float dx = pBody.X - proj.X;
+                        float dy = pBody.Y - proj.Y;
+                        points = points.Select(p => new PointF(p.X + dx, p.Y + dy)).ToArray();
+                    }
+                }
                 g.DrawCurve(penGrid, points);
             }
+        }
+
+        private PointF getProjectedPointOnLineFast(PointF v1, PointF v2, PointF p0)
+        {
+            // get dot product of e1, e2
+            PointF e1 = new PointF(v2.X - v1.X, v2.Y - v1.Y);
+            PointF e2 = new PointF(p0.X - v1.X, p0.Y - v1.Y);
+
+
+            double val = dotProduct(e1, e2);
+
+            // get squared length of e1
+            double len2 = e1.X * e1.X + e1.Y * e1.Y;
+            PointF p = new PointF((float)(v1.X + (val * e1.X) / len2),
+                                 (float)(v1.Y + (val * e1.Y) / len2));
+            return p;
+        }
+
+        private float dotProduct(PointF p1, PointF p2)
+        {
+            return (p1.X * p2.X + p1.Y * p2.Y);
         }
     }
 }
