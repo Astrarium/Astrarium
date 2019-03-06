c512701b8e5a4c6f7dff6ef2e82b569f745383cc
Alexander Krutov <a.a.krutov@gmail.com>
2019-03-06 23:50:35 +0300

    Added window style

---
+++ Planetarium/Themes/WindowStyle.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Planetarium.Themes
+{
+    public partial class WindowStyle : ResourceDictionary
+    {
+        public WindowStyle()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseClick(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.Close();
+        }
+
+        private void MaximizeRestoreClick(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            if (window.WindowState == System.Windows.WindowState.Normal)
+            {
+                window.WindowState = System.Windows.WindowState.Maximized;
+            }
+            else
+            {
+                window.WindowState = System.Windows.WindowState.Normal;
+            }
+        }
+
+        private void MinimizeClick(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.WindowState = System.Windows.WindowState.Minimized;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello!");
+        }
+    }
+}
