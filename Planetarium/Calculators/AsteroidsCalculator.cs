adbcbec8346182798143b768ca91f298c792dba7
Alexander Krutov <a.a.krutov@gmail.com>
2019-05-27 21:40:30 +0300

    Calculating asteroids positions

--- Planetarium/Calculators/AsteroidsCalculator.cs
+++ Planetarium/Calculators/AsteroidsCalculator.cs
@@ -15,70 +15,25 @@ namespace Planetarium.Calculators
         ICollection<Asteroid> Asteroids { get; }
     }
 
-    public class AsteroidsCalculator : BaseCalc, IAsteroidsProvider
+    public class AsteroidsCalculator : BaseCalc, ICelestialObjectCalc<Asteroid>, IAsteroidsProvider
     {
         private readonly string ORBITAL_ELEMENTS_FILE = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Data/Asteroids.dat");
 
         private AsteroidsReader reader = new AsteroidsReader();
 
-        public ICollection<Asteroid> Asteroids { get; private set; }
+        private List<Asteroid> asteroids = new List<Asteroid>();
+        public ICollection<Asteroid> Asteroids => asteroids;
 
         public override void Initialize()
         {
-            Asteroids = reader.Read(ORBITAL_ELEMENTS_FILE);
+            asteroids.AddRange(reader.Read(ORBITAL_ELEMENTS_FILE));
         }
 
         public override void Calculate(SkyContext c)
         {
             for (int i = 0; i < Asteroids.Count; i++)
             {
-                // final difference to stop iteration process, 1 second of time
-                double deltaTau = TimeSpan.FromSeconds(1).TotalDays;
-
-                // time taken by the light to reach the Earth
-                double tau = 0;
-
-                // previous value of tau to calculate the difference
-                double tau0 = 1;
-
-                // Rectangular coordinates of asteroid
-                CrdsRectangular rect = null;
-
-                // Rectangular coordinates of sun
-                var sun = c.Get(SunRectangular);
-
-                double ksi = 0, eta = 0, zeta = 0, Delta = 0;
-
-                // Iterative process to find heliocentrical coordinates of planet
-                while (Math.Abs(tau - tau0) > deltaTau)
-                {
-                    // Rectangular coordinates of asteroid
-                    rect = MinorBodyPositions.GetRectangularCoordinates(Asteroids.ElementAt(i).Orbit, c.JulianDay - tau, c.Epsilon);
-
-                    ksi = sun.X + rect.X;
-                    eta = sun.Y + rect.Y;
-                    zeta = sun.Z + rect.Z;
-
-                    // Distance to the Earth
-                    Delta = Math.Sqrt(ksi * ksi + eta * eta + zeta * zeta);
-
-                    tau0 = tau;
-                    tau = PlanetPositions.LightTimeEffect(Delta);
-                }
-
-                double alpha = Angle.ToDegrees(Math.Atan2(eta, ksi));
-                double delta = Angle.ToDegrees(Math.Asin(zeta / Delta));
-
-                var eq = new CrdsEquatorial(alpha, delta);
-
-                // Nutation effect
-                var eq1 = Nutation.NutationEffect(eq, c.NutationElements, c.Epsilon);
-
-                // Aberration effect
-                var eq2 = Aberration.AberrationEffect(eq, c.AberrationElements, c.Epsilon);
-
-                // Apparent coordinates of the object
-                eq += eq1 + eq2;
+                var eq = c.Get(Equatorial, i);
 
                 // Apparent horizontal coordinates
                 Asteroids.ElementAt(i).Horizontal = eq.ToHorizontal(c.GeoLocation, c.SiderealTime);
@@ -87,6 +42,58 @@ namespace Planetarium.Calculators
             }
         }
 
+        private CrdsEquatorial Equatorial(SkyContext c, int i)
+        {
+            // final difference to stop iteration process, 1 second of time
+            double deltaTau = TimeSpan.FromSeconds(1).TotalDays;
+
+            // time taken by the light to reach the Earth
+            double tau = 0;
+
+            // previous value of tau to calculate the difference
+            double tau0 = 1;
+
+            // Rectangular coordinates of asteroid
+            CrdsRectangular rect = null;
+
+            // Rectangular coordinates of sun
+            var sun = c.Get(SunRectangular);
+
+            double ksi = 0, eta = 0, zeta = 0, Delta = 0;
+
+            // Iterative process to find heliocentrical coordinates of planet
+            while (Math.Abs(tau - tau0) > deltaTau)
+            {
+                // Rectangular coordinates of asteroid
+                rect = MinorBodyPositions.GetRectangularCoordinates(Asteroids.ElementAt(i).Orbit, c.JulianDay - tau, c.Epsilon);
+
+                ksi = sun.X + rect.X;
+                eta = sun.Y + rect.Y;
+                zeta = sun.Z + rect.Z;
+
+                // Distance to the Earth
+                Delta = Math.Sqrt(ksi * ksi + eta * eta + zeta * zeta);
+
+                tau0 = tau;
+                tau = PlanetPositions.LightTimeEffect(Delta);
+            }
+
+            double alpha = Angle.ToDegrees(Math.Atan2(eta, ksi));
+            double delta = Angle.ToDegrees(Math.Asin(zeta / Delta));
+
+            var eq = new CrdsEquatorial(alpha, delta);
+
+            // Nutation effect
+            var eq1 = Nutation.NutationEffect(eq, c.NutationElements, c.Epsilon);
+
+            // Aberration effect
+            var eq2 = Aberration.AberrationEffect(eq, c.AberrationElements, c.Epsilon);
+
+            // Apparent coordinates of the object
+            eq += eq1 + eq2;
+
+            return eq;
+        }
 
         /// <summary>
         /// Gets rectangular coordinates of Sun
@@ -109,6 +116,40 @@ namespace Planetarium.Calculators
             //sunEcliptical += Aberration.AberrationEffect(sunEcliptical.Distance);
 
             return eSun.ToRectangular(c.Epsilon);
+        }
+
+        public void ConfigureEphemeris(EphemerisConfig<Asteroid> config)
+        {
+            
+        }
+
+        public CelestialObjectInfo GetInfo(SkyContext c, Asteroid body)
+        {
+            int i = asteroids.IndexOf(body);
+
+            var info = new CelestialObjectInfo();
+
+            info.SetSubtitle("Minor planet").SetTitle(GetName(body))
+
+            //.AddRow("Constellation", Constellations.FindConstellation(c.Get(JupiterMoonEquatorial, m), c.JulianDay))
+
+            .AddHeader("Equatorial coordinates (geocentrical)")
+            .AddRow("Equatorial.Alpha", c.Get(Equatorial, i).Alpha)
+            .AddRow("Equatorial.Delta", c.Get(Equatorial, i).Delta);
+
+            return info;
+        }
+
+        public ICollection<SearchResultItem> Search(string searchString, int maxCount = 50)
+        {
+            return Asteroids
+                .Where(a => a.Name.StartsWith(searchString, StringComparison.OrdinalIgnoreCase))
+                .Select(p => new SearchResultItem(p, p.Name)).ToArray();
+        }
+
+        public string GetName(Asteroid body)
+        {
+            return body.Name;
         }
     }
 }
