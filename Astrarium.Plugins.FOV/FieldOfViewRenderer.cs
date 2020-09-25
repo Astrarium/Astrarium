39a77c81c306f2322e8ac0e49bda6a13c3d43b54
Alexander Krutov <a.a.krutov@gmail.com>
2020-09-25 11:40:21 +0300

    FOV plugin: enhancements in rendering

--- Astrarium.Plugins.FOV/FieldOfViewRenderer.cs
+++ Astrarium.Plugins.FOV/FieldOfViewRenderer.cs
@@ -30,63 +30,71 @@ namespace Astrarium.Plugins.FOV
 
             foreach (var frame in frames)
             {
-                if (frame is TelescopeFovFrame telescopeFovFrame)
+                if (frame is CircularFovFrame circularFrame)
                 {
-                    float radius = telescopeFovFrame.Size * 3600 / 2;
+                    float radius = circularFrame.Size * 3600 / 2;
                     float size = map.GetDiskSize(radius);
 
-                    if (frame.Shading > 0 && telescopeFovFrame.Size >= map.ViewAngle / 2)
+                    // do not draw frame if its size exceeds screen bounds
+                    if (size < Math.Max(map.Width, map.Height))
                     {
-                        var circle = new GraphicsPath();
-                        circle.AddEllipse(map.Width / 2 - size / 2, map.Height / 2 - size / 2, size, size);
+                        if (frame.Shading > 0 && circularFrame.Size >= map.ViewAngle / 2)
+                        {
+                            var circle = new GraphicsPath();
+                            circle.AddEllipse(map.Width / 2 - size / 2, map.Height / 2 - size / 2, size, size);
 
-                        var shading = new Region(new RectangleF(0, 0, map.Width, map.Height));
-                        shading.Exclude(circle);
+                            var shading = new Region(new RectangleF(0, 0, map.Width, map.Height));
+                            shading.Exclude(circle);
 
-                        int transparency = (int)(frame.Shading / 100f * 255);
-                        var solidBrush = new SolidBrush(Color.FromArgb(transparency, map.GetSkyColor()));
-                        map.Graphics.FillRegion(solidBrush, shading);
-                    }
-                    map.Graphics.DrawEllipse(new Pen(frame.Color), map.Width / 2 - size / 2, map.Height / 2 - size / 2, size, size);
+                            int transparency = (int)(frame.Shading / 100f * 255);
+                            var solidBrush = new SolidBrush(Color.FromArgb(transparency, map.GetSkyColor()));
+                            map.Graphics.FillRegion(solidBrush, shading);
+                        }
+                        map.Graphics.DrawEllipse(new Pen(frame.Color), map.Width / 2 - size / 2, map.Height / 2 - size / 2, size, size);
 
-                    float labelWidth = map.Graphics.MeasureString(frame.Label, font).Width;
-                    if (labelWidth <= size * 2)
-                    {
-                        map.Graphics.DrawString(frame.Label, font, new SolidBrush(frame.Color), new PointF(map.Width / 2, map.Height / 2 + size / 2), format);
+                        float labelWidth = map.Graphics.MeasureString(frame.Label, font).Width;
+                        if (labelWidth <= size * 2)
+                        {
+                            map.Graphics.DrawString(frame.Label, font, new SolidBrush(frame.Color), new PointF(map.Width / 2, map.Height / 2 + size / 2), format);
+                        }
                     }
                 }
-                else if (frame is CameraFovFrame cameraFovFrame)
+                else if (frame is CameraFovFrame cameraFrame)
                 {
-                    float width = map.GetDiskSize(cameraFovFrame.Width * 3600);
-                    float height = map.GetDiskSize(cameraFovFrame.Height * 3600);
+                    float width = map.GetDiskSize(cameraFrame.Width * 3600);
+                    float height = map.GetDiskSize(cameraFrame.Height * 3600);
 
-                    var eqCenter = map.Center.ToEquatorial(map.GeoLocation, map.SiderealTime);
-                    float angle = map.GetRotationTowardsNorth(eqCenter) + cameraFovFrame.Rotation;
- 
-                    map.Graphics.TranslateTransform(map.Width / 2, map.Height / 2);
-                    map.Graphics.RotateTransform(angle);
-
-                    if (frame.Shading > 0 && cameraFovFrame.Height >= map.ViewAngle / 2)
+                    // do not draw frame if its size exceeds screen bounds
+                    if (Math.Min(width, height) < Math.Max(map.Width, map.Height))
                     {
-                        var rect = new GraphicsPath();
-                        rect.AddRectangle(new RectangleF(-width / 2, -height / 2, width, height));
+                        var eqCenter = map.Center.ToEquatorial(map.GeoLocation, map.SiderealTime);
+                        float angle = map.GetRotationTowardsNorth(eqCenter) + cameraFrame.Rotation;
 
-                        var shading = new Region(new RectangleF(0, 0, map.Width, map.Height));
-                        shading.Exclude(rect);
+                        map.Graphics.TranslateTransform(map.Width / 2, map.Height / 2);
+                        map.Graphics.RotateTransform(angle);
 
-                        int transparency = (int)(frame.Shading / 100f * 255);
-                        var solidBrush = new SolidBrush(Color.FromArgb(transparency, map.GetSkyColor()));
-                        map.Graphics.FillRegion(solidBrush, shading);
+                        if (frame.Shading > 0 && cameraFrame.Height >= map.ViewAngle / 2)
+                        {
+                            var rect = new GraphicsPath();
+                            rect.AddRectangle(new RectangleF(-width / 2, -height / 2, width, height));
+
+                            var shading = new Region(new RectangleF(0, 0, map.Width, map.Height));
+                            shading.Exclude(rect);
+
+                            int transparency = (int)(frame.Shading / 100f * 255);
+                            var solidBrush = new SolidBrush(Color.FromArgb(transparency, map.GetSkyColor()));
+                            map.Graphics.FillRegion(solidBrush, shading);
+                        }
+
+                        map.Graphics.DrawRectangle(new Pen(frame.Color), -width / 2, -height / 2, width, height);
+                        float labelWidth = map.Graphics.MeasureString(frame.Label, font).Width;
+                        if (labelWidth <= width * 2)
+                        {
+                            map.Graphics.DrawString(frame.Label, font, new SolidBrush(frame.Color), new PointF(0, height / 2), format);
+                        }
+
+                        map.Graphics.ResetTransform();
                     }
-
-                    map.Graphics.DrawRectangle(new Pen(frame.Color), -width / 2, -height / 2, width, height);
-                    float labelWidth = map.Graphics.MeasureString(frame.Label, font).Width;
-                    if (labelWidth <= width * 2)
-                    {
-                        map.Graphics.DrawString(frame.Label, font, new SolidBrush(frame.Color), new PointF(0, height / 2), format);
-                    }
-
-                    map.Graphics.ResetTransform();
                 }
             }
 
