46ab026acf51cd24f95e3ec9d40e351e10734ff8
Alexander Krutov <a.a.krutov@gmail.com>
2023-09-26 18:59:53 +0300

    Implemented #111
    Allow to export astronomical phenomena to iCal and Google Calendar

--- Astrarium/ViewModels/PhenomenaVM.cs
+++ Astrarium/ViewModels/PhenomenaVM.cs
@@ -3,8 +3,12 @@ using Astrarium.Types;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Text;
 using System.Windows;
 
 namespace Astrarium.ViewModels
@@ -19,7 +23,15 @@ namespace Astrarium.ViewModels
 
         private ICollection<AstroEvent> events;
         private readonly ISky sky;
-        
+
+        private readonly Dictionary<string, Type> exportFormats = new Dictionary<string, Type>()
+        {
+            ["Text file(*.txt)|*.txt"] = typeof(AstroEventsTextWriter),
+            ["Comma-separated file with Julian dates (*.csv)|*.csv"] = typeof(AstroEventsGenericCsvWriter),
+            ["Comma-separated file suitable for Google Calendar (*.csv)|*.csv"] = typeof(AstroEventsGoogleCalendarCsvWriter),
+            ["iCalendar file (*.ics)|*.ics"] = typeof(AstroEventsICalendarWriter),
+        };
+
         public PhenomenaVM(ISky sky)
         {
             this.sky = sky;
@@ -40,26 +52,21 @@ namespace Astrarium.ViewModels
 
         public bool NoEvents => !Events.Any();
 
+
+        private Type GetExportFormat(int index)
+        {
+            return exportFormats.ElementAt(index - 1).Value;
+        }
+
         private void SaveToFile()
         {
-            var file = ViewManager.ShowSaveFileDialog(Text.Get("PhenomenaWindow.ExportTitle"), "Phenomena", ".csv", "Text files (*.txt)|*.txt|Comma-separated files (*.csv)|*.csv", out int selectedFilterIndex);
+            var file = ViewManager.ShowSaveFileDialog(Text.Get("PhenomenaWindow.ExportTitle"), "Phenomena", ".ics", string.Join("|", exportFormats.Keys), out int selectedFilterIndex);
             if (file != null)
             {
                 IAstroEventsWriter writer = null;
-                string ext = Path.GetExtension(file);
-                switch (ext)
-                {
-                    case ".csv":
-                        writer = new AstroEventsCsvWriter(file, sky.Context.GeoLocation.UtcOffset);
-                        break;
-                    case ".txt":
-                        writer = new AstroEventsTextWriter(file, sky.Context.GeoLocation.UtcOffset);
-                        break;
-                    default:
-                        break;
-                }
-
-                writer?.Write(events);
+                Type format = GetExportFormat(selectedFilterIndex);
+                writer = (IAstroEventsWriter)Activator.CreateInstance(format);
+                writer?.Write(events, file, sky.Context.GeoLocation.UtcOffset);
 
                 var answer = ViewManager.ShowMessageBox("$PhenomenaWindow.ExportDoneTitle", "$PhenomenaWindow.ExportDoneText", MessageBoxButton.YesNo);
                 if (answer == MessageBoxResult.Yes)
@@ -100,21 +107,12 @@ namespace Astrarium.ViewModels
 
     public interface IAstroEventsWriter
     {
-        void Write(ICollection<AstroEvent> events);
+        void Write(ICollection<AstroEvent> events, string file, double utcOffset);
     }
 
     public class AstroEventsTextWriter : IAstroEventsWriter
     {
-        private string file;
-        private double utcOffset;
-
-        public AstroEventsTextWriter(string file, double utcOffset)
-        {
-            this.file = file;
-            this.utcOffset = utcOffset;
-        }
-
-        public void Write(ICollection<AstroEvent> events)
+        public void Write(ICollection<AstroEvent> events, string file, double utcOffset)
         {
             using (var writer = File.CreateText(file))
             {
@@ -127,22 +125,13 @@ namespace Astrarium.ViewModels
                 }
                 writer.Flush();
                 writer.Close();
-            }            
+            }
         }
     }
 
-    public class AstroEventsCsvWriter : IAstroEventsWriter
+    public class AstroEventsGenericCsvWriter : IAstroEventsWriter
     {
-        private string file;
-        private double utcOffset;
-
-        public AstroEventsCsvWriter(string file, double utcOffset)
-        {
-            this.file = file;
-            this.utcOffset = utcOffset;
-        }
-
-        public void Write(ICollection<AstroEvent> events)
+        public void Write(ICollection<AstroEvent> events, string file, double utcOffset)
         {
             using (var writer = File.CreateText(file))
             {
@@ -153,6 +142,74 @@ namespace Astrarium.ViewModels
                     if (!e.NoExactTime) dateTimeString += $" {Formatters.Time.Format(date)}";
                     writer.WriteLine($"\"{e.JulianDay}\",\"{dateTimeString}\",\"{e.Text}\"");
                 }
+                writer.Flush();
+                writer.Close();
+            }
+        }
+    }
+
+    public class AstroEventsGoogleCalendarCsvWriter : IAstroEventsWriter
+    {
+        public void Write(ICollection<AstroEvent> events, string file, double utcOffset)
+        {
+            using (var writer = File.CreateText(file))
+            {
+                // header
+                writer.WriteLine($"\"Subject\",\"Start Date\",\"Start Time\",\"End Date\",\"End Time\",\"All Day Event\",\"Description\"");
+
+                foreach (var e in events)
+                {
+                    DateTime date = new Date(e.JulianDay, utcOffset).ToDateTime();
+                    string text = e.Text;
+                    string startDate = date.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture);
+                    string startTime = e.NoExactTime ? "" : date.ToString("hh:mm tt", CultureInfo.InvariantCulture);
+                    string endDate = e.NoExactTime ? "" : startDate;
+                    string endTime = e.NoExactTime ? "" : startTime;
+                    string allDay = e.NoExactTime ? "True" : "False";
+                    string[] columns = new string[] { text, startDate, startTime, endDate, endTime, allDay, text };
+                    writer.WriteLine(string.Join(",", columns.Select(x => $"\"{x}\"")));
+                }
+                writer.Flush();
+                writer.Close();
+            }
+        }
+    }
+
+    public class AstroEventsICalendarWriter : IAstroEventsWriter
+    {
+        public void Write(ICollection<AstroEvent> events, string file, double utcOffset)
+        {
+            using (var writer = File.CreateText(file))
+            {
+                string appVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+                string created = DateTime.UtcNow.ToString("yyyyMMddTHHmmss");
+
+                writer.WriteLine("BEGIN:VCALENDAR");
+                writer.WriteLine("VERSION:2.0");
+                writer.WriteLine($"PRODID:-//Astrarium/Astrarium {appVersion}//EN");
+                writer.WriteLine("X-WR-CALNAME:Astronomical phenomena");
+                writer.WriteLine("X-WR-TIMEZONE:UTC");
+                writer.WriteLine("X-WR-CALDESC:Astronomical phenomena");
+
+                foreach (var e in events)
+                {
+                    DateTime localDate = new Date(e.JulianDay, utcOffset).ToDateTime();
+                    string utcTimestamp = $"{TimeZoneInfo.ConvertTimeToUtc(localDate):yyyyMMddTHHmmss}Z";
+                    string startDate = e.NoExactTime ? localDate.ToString("yyyyMMdd") : utcTimestamp;
+                    string endDate = e.NoExactTime ? localDate.AddDays(1).ToString("yyyyMMdd") : utcTimestamp;
+                    string uid = Guid.NewGuid().ToString();
+                    writer.WriteLine("BEGIN:VEVENT");
+                    writer.WriteLine($"DTSTAMP:{created}");
+                    writer.WriteLine($"DTSTART:{startDate}");
+                    writer.WriteLine($"DTEND:{endDate}");
+                    writer.WriteLine($"UID:{uid}");
+                    writer.WriteLine($"DESCRIPTION:{e.Text}");
+                    writer.WriteLine($"SUMMARY:{e.Text}");
+                    writer.WriteLine("END:VEVENT");
+                }
+
+                writer.WriteLine("END:VCALENDAR");
+
                 writer.Flush();
                 writer.Close();
             }
