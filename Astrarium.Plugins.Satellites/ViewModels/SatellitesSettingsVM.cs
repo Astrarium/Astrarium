6a00b315d6384fe8c30a5e2fb577bdaf747b6311
Alexander Krutov <krutov@tecomgroup.ru>
2024-06-04 16:36:11 +0300

    Satellites plugin: settings (development)

--- Astrarium.Plugins.Satellites/ViewModels/SatellitesSettingsVM.cs
+++ Astrarium.Plugins.Satellites/ViewModels/SatellitesSettingsVM.cs
@@ -1,6 +1,7 @@
 ﻿using Astrarium.Types;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,30 @@ namespace Astrarium.Plugins.Satellites.ViewModels
 {
     public class SatellitesSettingsVM : SettingsViewModel
     {
+        private ISettings settings;
+
+        public ICommand DeleteCommand { get; private set; }
+
+        public ObservableCollection<TLESource> TLESources { get; private set; }
+
         public SatellitesSettingsVM(ISettings settings) : base(settings)
         {
+            this.settings = settings;
 
+            TLESources = new ObservableCollection<TLESource>(settings.Get<List<TLESource>>("SatellitesOrbitalElements"));
+
+            DeleteCommand = new Command<object>(Delete);
+        }
+
+        private void Delete(object parameter)
+        {
+            if (parameter is TLESource tleSource)
+            {
+                if (ViewManager.ShowMessageBox("$Warning", "Do you really want to delete TLE source?", System.Windows.MessageBoxButton.YesNo) == System.Windows.MessageBoxResult.Yes)
+                {
+                    TLESources.Remove(tleSource);
+                }
+            }
         }
     }
 }
