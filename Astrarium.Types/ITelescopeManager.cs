e0849e182a2ac7ba853d399c7cf7c27b934c830b
Alexander Krutov <a.a.krutov@gmail.com>
2026-04-09 21:42:21 +0300

    TelescopeManagerStub now is along with ITelescopeManager

--- Astrarium.Types/ITelescopeManager.cs
+++ Astrarium.Types/ITelescopeManager.cs
@@ -13,4 +13,15 @@ namespace Astrarium.Types
         bool IsTelescopeConnected { get; }
         event Action TelescopeConnectionChanged;
     }
+
+    /// <summary>
+    /// Default stub for the interface, in case when telescope control is not available
+    /// </summary>
+    public class TelescopeManagerStub : ITelescopeManager
+    {
+        public bool IsTelescopeAvailable => false;
+        public bool IsTelescopeConnected => false;
+        public event Action TelescopeConnectionChanged;
+        public void SlewToCoordinates(CrdsEquatorial eq) { }
+    }
 }
