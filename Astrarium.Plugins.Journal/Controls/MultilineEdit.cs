fd384933a39f655af43975661640cdabdaa24136
Alexander Krutov <a.a.krutov@gmail.com>
2026-04-17 23:31:06 +0300

    Journal plugin (styles)

--- Astrarium.Plugins.Journal/Controls/MultilineEdit.cs
+++ Astrarium.Plugins.Journal/Controls/MultilineEdit.cs
@@ -120,7 +120,7 @@ namespace Astrarium.Plugins.Journal.Controls
 
         private void _placeholder_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
+            if (e.LeftButton == MouseButtonState.Pressed && e.ClickCount > 1)
             {
                 SetEditMode();
             }
